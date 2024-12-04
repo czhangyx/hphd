4980c8c9aef76fcca57d3c4bcdd42d3c9287922c
romaankolekar <91655723+romaankolekar@users.noreply.github.com>
2024-12-04 14:12:38 -0800

    force override fixed

--- jbrowse_upload.sh
+++ jbrowse_upload.sh
@@ -19,11 +19,11 @@ wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/858/285/GCF_000858285.1_Vi
 
 gunzip GCF_000858285.1_ViralProj15198_genomic.gff.gz 
 
-jbrowse sort-gff GCF_000858285.1_ViralProj15198_genomic.gff.gz > genes.gff --force #to override and add new tracks
+jbrowse sort-gff GCF_000858285.1_ViralProj15198_genomic.gff.gz > genes.gff  #to override and add new tracks
 bgzip genes.gff
 tabix genes.gff.gz
 
-jbrowse add-track genes.gff.gz --out $APACHE_ROOT/jbrowse2 --load copy --assemblyNames=vzv.fna #load annotation tracks to jbrowse
+jbrowse add-track genes.gff.gz --out $APACHE_ROOT/jbrowse2 --load copy --assemblyNames=vzv.fna --force #load annotation tracks to jbrowse
 
 jbrowse text-index --out $APACHE_ROOT/jbrowse2 #index for search by gene
 
