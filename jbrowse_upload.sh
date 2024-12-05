49a82661b25658f25614c49be518db8dc733e747
romaankolekar <91655723+romaankolekar@users.noreply.github.com>
2024-12-04 17:33:12 -0800

    Update jbrowse_upload.sh

--- jbrowse_upload.sh
+++ jbrowse_upload.sh
@@ -18,37 +18,37 @@ while IFS= read -r line; do
 
                         ####Downloading Data Section and uploading to Jbrowse#####
 
-                        wget $ref_genome #download genome
+                        wget "$ref_genome" #download genome
 
                         ref_genome_name=$(basename "$ref_genome")
 
-                        gunzip $ref_genome_name #unzip ref genome
+                        gunzip "$ref_genome_name" #unzip ref genome
 
                         ref_genome_name_fna=${ref_genome_name%.gz}
 
-                        mv $ref_genome_name_fna $assembly #rename
+                        mv "$ref_genome_name_fna" "$assembly" #rename
 
-                        samtools faidx $assembly #index ref genome
+                        samtools faidx "$assembly" #index ref genome
 
-                        jbrowse add-assembly $assembly --out $APACHE_ROOT/jbrowse2 --load copy #load into jbrowse
+                        jbrowse add-assembly "$assembly" --out $APACHE_ROOT/jbrowse2 --load copy #load into jbrowse
 
 
                         #get genome annotations
 
-                        wget $genes
+                        wget "$genes"
 
                         genes_name=$(basename "$genes")
 
-                        gunzip $genes_name
+                        gunzip "$genes_name"
 
                         track="gene_${assembly}.gff"
 
-                        jbrowse sort-gff $genes_name > $track #to override and add new tracks
-                        bgzip $track
+                        jbrowse sort-gff "$genes_name" > "$track" #to override and add new tracks
+                        bgzip "$track"
                         track_gz="${track}.gz"
-                        tabix $track_gz
+                        tabix "$track_gz"
 
-                        jbrowse add-track $track_gz --out $APACHE_ROOT/jbrowse2 --load copy --assemblyNames=$assembly #load annotation tracks to jbrowse
+                        jbrowse add-track "$track_gz" --out $APACHE_ROOT/jbrowse2 --load copy --assemblyNames="$assembly" #load annotation tracks to jbrowse
 
                         jbrowse text-index --out $APACHE_ROOT/jbrowse2 #index for search by gene
                  fi
@@ -56,7 +56,7 @@ while IFS= read -r line; do
 
         else
                 if [ -z "$gene_accession_ref" ]; then
-                        gene_accession_ref=$line
+                        gene_accession_ref="$line"
                 elif [ -z "$ref_genome" ]; then
                         ref_genome="$line"
                 elif [ -z "$gene_accession_gene" ]; then
