c4253788cdb7a47dc56605371207c9ce46cd125c
romaankolekar <91655723+romaankolekar@users.noreply.github.com>
2024-12-04 17:53:46 -0800

    Update jbrowse_upload.sh

--- jbrowse_upload.sh
+++ jbrowse_upload.sh
@@ -11,62 +11,75 @@ gene_accession_gene=""
 genes=""
 assembly=""
 
-while IFS= read -r line; do
-        if [ -z "$line" ]; then
+counter=0
+
+while IFS= read -r line || [ -n "$line" ]; do
+        if [[ -z "$line" || "$line" =~ ^[[:space:]]*$ ]]; then
                 continue
         fi
+
+        ((counter++)) #want to make sure we read 5 lines at a time
+
+        case "$counter" in
+                1) gene_accession_ref="$line" ;;
+                2) ref_genome="$line" ;;
+                3) gene_accession_gene="$line" ;;
+                4) genes="$line" ;;
+                5) 
+                        assembly="$line" ;;
         
-        if [ -n "$gene_accession" ] && [ -n "$gene_accession_ref" ] &&  [ -n "$ref_genome" ] && [ -n "$genes" ]  && [ -n "$gene_accession_gene" ]; then
-                echo "Complete Genome $gene_accession_ref being uploaded"
+                        echo "Complete Genome Accession Reference: $gene_accession_ref"
+                        echo "Reference Genome ftp link: $ref_genome"
+                        echo "Gene annotations accession Reference: $gene_accession_gene"
+                        echo "Gene annotations gff files: $genes"
+                        echo "Assembly Name: $assembly"
 
                         ####Downloading Data Section and uploading to Jbrowse#####
 
-                wget "$ref_genome" #download genome
+                        wget "$ref_genome" #download genome
 
-                ref_genome_name=$(basename "$ref_genome")
+                        ref_genome_name=$(basename "$ref_genome")
 
-                gunzip "$ref_genome_name" #unzip ref genome
+                        gunzip "$ref_genome_name" #unzip ref genome
 
-                ref_genome_name_fna=${ref_genome_name%.gz}
+                        ref_genome_name_fna=${ref_genome_name%.gz}
 
-                mv "$ref_genome_name_fna" "$assembly" #rename
+                        mv "$ref_genome_name_fna" "$assembly" #rename
 
-                samtools faidx "$assembly" #index ref genome
+                        samtools faidx "$assembly" #index ref genome
 
-                jbrowse add-assembly "$assembly" --out $APACHE_ROOT/jbrowse2 --load copy #load into jbrowse
+                        jbrowse add-assembly "$assembly" --out $APACHE_ROOT/jbrowse2 --load copy #load into jbrowse
 
 
                         #get genome annotations
 
-                wget "$genes"
+                        wget "$genes"
 
-                genes_name=$(basename "$genes")
+                        genes_name=$(basename "$genes")
 
-                gunzip "$genes_name"
+                        gunzip "$genes_name"
 
-                track="gene_${assembly}.gff"
+                        track="gene_${assembly}.gff"
 
-                jbrowse sort-gff "$genes_name" > "$track" #to override and add new tracks
-                bgzip "$track"
-                track_gz="${track}.gz"
-                tabix "$track_gz"
+                        jbrowse sort-gff "$genes_name" > "$track" #to override and add new tracks
+                        bgzip "$track"
+                        track_gz="${track}.gz"
+                        tabix "$track_gz"
 
-                jbrowse add-track "$track_gz" --out $APACHE_ROOT/jbrowse2 --load copy --assemblyNames="$assembly" #load annotation tracks to jbrowse
+                        jbrowse add-track "$track_gz" --out $APACHE_ROOT/jbrowse2 --load copy --assemblyNames="$assembly" #load annotation tracks to jbrowse
 
-                jbrowse text-index --out $APACHE_ROOT/jbrowse2 #index for search by gene
-         fi
-       
-         if [ -z "$gene_accession_ref" ]; then
-                gene_accession_ref="$line"
-         elif [ -z "$ref_genome" ]; then
-                ref_genome="$line"
-         elif [ -z "$gene_accession_gene" ]; then
-                gene_accession_gene="$line"
-         elif [ -z "$genes" ]; then
-                genes="$line"
-         else 
-                assembly="$line"
-         fi
+                        jbrowse text-index --out $APACHE_ROOT/jbrowse2 #index for search by gene
+
+                        gene_accession_ref=""
+                        ref_genome=""
+                        gene_accession_gene=""
+                        genes=""
+                        assembly=""
+
+                        counter=0
+                        ;;
+                esac
+        
 #make sure the file ends with an empty line
 done < "$FILE"
 
